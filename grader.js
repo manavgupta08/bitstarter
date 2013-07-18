88bf455762a4d33347032c8b4adfde37c0b42473
manavgupta08 <manav.gupta07@gmail.com>
2013-07-18 05:50:00 +0000

    updated graderjs

--- grader.js
+++ grader.js
@@ -27,6 +27,11 @@ var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
+var sys = require('util'); 
+var rest = require('restler');
+var URL_DEFAULT = "http://infinite-badlands-5939.herokuapp.com/";
+var assertUrlExists = function (val) {return val.toString();}; 
+
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
@@ -65,10 +70,24 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'url to check', clone(assertUrlExists), URL_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    
+if (program.url) {
+    rest.get(program.url).on('complete', function(result) {
+      fs.writeFileSync("myfile.html", result);   // Added this line
+      var checkJson = checkHtmlFile("myfile.html", program.checks);
+      var outJson = JSON.stringify(checkJson, null, 4);
+      console.log(outJson);
+    });
+
+} else {
+  var checkJson = checkHtmlFile(program.file, program.checks);
+  var outJson = JSON.stringify(checkJson, null, 4);
+  console.log(outJson);
+
+}
+
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
